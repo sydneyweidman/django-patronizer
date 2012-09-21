94fe3eedcc3f83863e52c14715fb86a63b915fa0
Sydney Weidman <sydney.weidman@gmail.com>
2012-09-21 15:46:18 -0500

    Bumping to v 0.2.1

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author_email = "sydney.weidman@gmail.com",
     packages = find_packages('src'),
     package_dir = { '': 'src'},
-    version="0.2",
+    version="0.2.1",
     install_requires = ['Django','pymarc'],
     )
