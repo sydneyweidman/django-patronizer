5811b23dca41fdc7f55f2016e14c53af0cc4338a
Sydney Weidman <sydney.weidman@gmail.com>
2012-09-25 09:15:02 -0500

    Bumping version to 0.2.3.

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author_email = "sydney.weidman@gmail.com",
     packages = find_packages('src'),
     package_dir = { '': 'src'},
-    version="0.2.2",
+    version="0.2.3",
     install_requires = ['Django','pymarc'],
     )
